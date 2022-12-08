22ee78b16024fc659af69766d2f3f70e7b849296
darkcode932 <atebederussel@gmail.com>
2022-12-08 11:05:19 +0100

    add login and register page

--- ChatApp/App.xaml.cs
+++ ChatApp/App.xaml.cs
@@ -1,13 +1,25 @@
-﻿using ChatApp.Views;
+﻿using Microsoft.Maui.Platform;
+using ChatApp.Views;
+using ChatApp.Handlers;
 
 namespace ChatApp;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-
-		MainPage = new NavigationPage(new HomeView());
-	}
+    public App()
+    {
+        InitializeComponent();
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(BorderlessEntry), (handler, view) =>
+        {
+            if (view is BorderlessEntry)
+            {
+#if __ANDROID__
+                handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+#elif __IOS__
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+            }
+        });
+        MainPage = new AppShell();
+    }
 }
